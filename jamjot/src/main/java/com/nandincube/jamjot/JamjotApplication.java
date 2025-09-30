99c806c6fbd010e3435b21466a5cef229a322005
nandincube <nandi_ncube@yahoo.com>
2025-09-30 17:10:22 +0200

    inital project structure creates and dependencies added

---
+++ jamjot/src/main/java/com/nandincube/jamjot/JamjotApplication.java
@@ -0,0 +1,13 @@
+package com.nandincube.jamjot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamjotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamjotApplication.class, args);
+	}
+
+}
---
+++ jamjot/src/test/java/com/nandincube/jamjot/JamjotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandincube.jamjot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamjotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
